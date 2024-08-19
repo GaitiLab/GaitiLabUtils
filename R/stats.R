874b765cc43f4fff25b59a568f3be59d6b37d6af
joan-yanqiong <Joan.Kant@uhn.ca>
2024-08-19 14:54:21 -0400

    add manually implemented t-test using LMMs + vis layer ggplot

--- R/stats.R
+++ R/stats.R
@@ -21,3 +21,34 @@ one_sample_perm_test_pval <- function(null_dist, true_val, alternative = c("less
     }
     return(pval)
 }
+
+
+
+#' perform a nested t-test using LMMs.
+#'
+#' @param data_df data frame containing data
+#' @param response name of column in data_df that corresponds to the response variable
+#' @param condition name of column in data_df that corresponds to condition of interest
+#' @param latent_vars vector containing columns in data_df that correspond to variables that describe the nested structure of the data e.g. Patient
+#'
+#' @return p-value derived from likelihood ratio test of model incorporating condition vs null model
+#' @export
+LMM_test <- function(data_df, response, condition, latent_vars) {
+    full_formula <- as.formula(
+        paste(response, "~", condition, "+", paste("(1 |", latent_vars, ")", collapse = "+"))
+    )
+
+    null_formula <- as.formula(
+        paste(response, "~ 1 +", paste("(1 |", latent_vars, ")", collapse = "+"))
+    )
+
+    # Fit the models
+    model <- lme4::lmer(full_formula, data = data_df)
+    model_null <- lme4::lmer(null_formula, data = data_df)
+
+    # Perform the likelihood ratio test
+    res <- anova(model_null, model)
+
+    # Return the p-value from the Chi-square test
+    return(res$`Pr(>Chisq)`[2])
+}
--- R/vis.R
+++ R/vis.R
@@ -191,3 +191,47 @@ create_hm <- function(
     )
     return(hm)
 }
+
+
+#' geom signif with lmm test. See geom_signif from ggsignif for more information
+#' 
+#' @param data_df data frame containing data
+#' @param response name of column in data_df that corresponds to the response variable
+#' @param condition name of column in data_df that corresponds to condition of interest
+#' @param latent_vars vector containing columns in data_df that correspond to variables that describe the nested structure of the data e.g. Patient
+#' @param comparisons list of vectors of length two that define the comparisons to make
+#' @param y_position position of brackets 
+#' @param tip_length length of bracket tips
+#' @param size size of brackets
+#' @param step_increase distance between brackets
+#' @return geom signif
+#' @examples 
+#' \dontrun {
+#' comparisons <- list(c("condition1", "condition2"), c("condition2", "condition3"))
+#' p <- ggplot(data = data_df, aes(x = condition, y = response, fill = condition)) + geom_boxplot() + geom_signif_lmm(data_df = data_df, response = "response", condition = "condition", latent_vars = "Patient", comparisons = comps, steps_increase = c(0, 0.1, 0.1))
+#' }
+#' @export
+geom_signif_lmm <- function(data_df, response, condition, latent_vars,
+    comparisons, y_position = NULL, tip_length = 0.03, size = 0.5, step_increase = 0) {
+
+    p_vals <- sapply(seq_along(comparisons), function(comp) {
+        
+        curr_comparison <- comparisons[[comp]]
+        curr_df <- data_df %>% 
+            filter(!!rlang::sym(condition) %in% curr_comparison)
+        
+        return(round(LMM_test(curr_df, response, condition, latent_vars), digits = 5))
+    })
+
+    return(
+        ggsignif::geom_signif(
+            comparisons = comparisons,
+            map_signif_level = FALSE,
+            annotations = p_vals,
+            y_position = y_position,
+            tip_length = tip_length,
+            size = size,
+            step_increase = step_increase
+        )
+    )
+}
