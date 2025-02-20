805fdbd7ac96ae1d4cf345e9b0ed404d974e8654
joan-yanqiong <Joan.Kant@uhn.ca>
2025-02-20 13:17:18 -0500

    update LMM implementation + vis - change the pval formatting to scientific and changed the rounding. - specify the REML parameter to FALSE

--- R/stats.R
+++ R/stats.R
@@ -21,15 +21,16 @@ one_sample_perm_test_pval <- function(null_dist, true_val, alternative = c("less
     }
     return(pval)
 }
-
-#' @title Perform a nested t-test using LMMs.
+#' perform a nested t-test using LMMs.
+#'
 #' @param data_df data frame containing data
 #' @param response name of column in data_df that corresponds to the response variable
 #' @param condition name of column in data_df that corresponds to condition of interest
 #' @param latent_vars vector containing columns in data_df that correspond to variables that describe the nested structure of the data e.g. Patient
+#' @param vebose (default=TRUE)
 #' @return p-value derived from likelihood ratio test of model incorporating condition vs null model
 #' @export
-LMM_test <- function(data_df, response, condition, latent_vars) {
+LMM_test <- function(data_df, response, condition, latent_vars, verbose = TRUE) {
     full_formula <- as.formula(
         paste(response, "~", condition, "+", paste("(1 |", latent_vars, ")", collapse = "+"))
     )
@@ -39,16 +40,19 @@ LMM_test <- function(data_df, response, condition, latent_vars) {
     )
 
     # Fit the models
-    model <- lme4::lmer(full_formula, data = data_df)
-    model_null <- lme4::lmer(null_formula, data = data_df)
+    model <- lme4::lmer(full_formula, data = data_df, REML = FALSE)
+    model_null <- lme4::lmer(null_formula, data = data_df, REML = FALSE)
 
     # Perform the likelihood ratio test
     res <- anova(model_null, model)
 
+    if (verbose) {
+        print(res)
+    }
+
     # Return the p-value from the Chi-square test
     return(res$`Pr(>Chisq)`[2])
 }
-
 #' @title Compute Jaccard similarity
 #' @param vec1 vector
 #' @param vec2 vector
--- R/vis.R
+++ R/vis.R
@@ -6,51 +6,72 @@
 auto_crop <- function(filename) {
     knitr::plot_crop(filename)
 }
-
 #' geom signif with lmm test. See geom_signif from ggsignif for more information
 #'
 #' @param data_df data frame containing data
 #' @param response name of column in data_df that corresponds to the response variable
 #' @param condition name of column in data_df that corresponds to condition of interest
+#' @param method one of the following 'nested_t_test' or 'mixed_effects_anova' (default="nested_t_test")
 #' @param latent_vars vector containing columns in data_df that correspond to variables that describe the nested structure of the data e.g. Patient
-#' @param comparisons list of vectors of length two that define the comparisons to make
+#' @param comparisons list of vectors of length two that define the comparisons to make (default=NULL)
 #' @param y_position position of brackets (default=NULL)
-#' @param tip_length length of bracket tips (default = 0.03)
-#' @param size size of brackets (default = 0.5)
-#' @param step_increase distance between brackets (default = 0)
+#' @param tip_length length of bracket tips (default=0.03)
+#' @param size size of brackets (default=0.5)
+#' @param step_increase distance between brackets (default=0)
+#' @param verbose (default=TRUE)
 #' @return geom signif
-#' @examples
-#' \dontrun{
-#' comparisons <- list(c("condition1", "condition2"), c("condition2", "condition3"))
-#' p <- ggplot(data = data_df, aes(x = condition, y = response, fill = condition)) +
-#'     geom_boxplot() +
-#'     geom_signif_lmm(data_df = data_df, response = "response", condition = "condition", latent_vars = "Patient", comparisons = comps, steps_increase = c(0, 0.1, 0.1))
-#' }
 #' @export
-#' @importFrom dplyr %>%
 geom_signif_lmm <- function(
-    data_df, response, condition, latent_vars,
-    comparisons, y_position = NULL,
-    tip_length = 0.03, size = 0.5, step_increase = 0) {
-    p_vals <- sapply(seq_along(comparisons), function(comp) {
-        curr_comparison <- comparisons[[comp]]
-        curr_df <- data_df %>%
-            filter(!!rlang::sym(condition) %in% curr_comparison)
+    data_df, response, condition, latent_vars, method = "nested_t_test",
+    comparisons = NULL, y_position = NULL, tip_length = 0.03, size = 0.5, step_increase = 0, verbose = TRUE) {
+    if (!method %in% c("nested_t_test", "mixed_effects_anova")) {
+        stop("Method needs to be nested_t_test or mixed_effects_anova")
+    }
 
-        return(round(LMM_test(curr_df, response, condition, latent_vars),
-            digits = 5
-        ))
-    })
+    if (method == "nested_t_test") {
+        p_vals <- sapply(seq_along(comparisons), function(comp) {
+            curr_comparison <- comparisons[[comp]]
+            curr_df <- data_df %>%
+                filter(!!dplyr::sym(condition) %in% curr_comparison)
 
-    return(
-        ggsignif::geom_signif(
-            comparisons = comparisons,
-            map_signif_level = FALSE,
-            annotations = p_vals,
-            y_position = y_position,
-            tip_length = tip_length,
-            size = size,
-            step_increase = step_increase
+            pval <- format(LMM_test(curr_df, response, condition, latent_vars), scientific = TRUE, digits = 5)
+            return(pval)
+        })
+
+        return(
+            ggsignif::geom_signif(
+                comparisons = comparisons,
+                map_signif_level = FALSE,
+                annotations = p_vals,
+                y_position = y_position,
+                tip_length = tip_length,
+                size = size,
+                step_increase = step_increase
+            )
         )
-    )
+    } else if (method == "mixed_effects_anova") {
+        if (!is.null(comparisons)) {
+            stop("Comparisons needs to be NULL for ANOVA")
+        }
+
+        if (class(data_df[[condition]]) != "factor") {
+            stop("X-axis variable needs to be factor")
+        }
+
+        middle_category <- levels(data_df[[condition]])[ceiling(length(levels(data_df[[condition]])) / 2)]
+
+        anova_pval <- format(LMM_test(data_df,
+            response, condition, latent_vars,
+            verbose = verbose
+        ), scientific = TRUE, digits = 5)
+        return(
+            ggplot2::annotate("text",
+                x = middle_category,
+                y = Inf,
+                label = paste("ANOVA:", anova_pval),
+                hjust = 0.5,
+                vjust = 1.5
+            )
+        )
+    }
 }
